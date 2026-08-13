919e504723ce55a579da6c968c4f3e17bb8b16e9
williamadriel72-code <williamadriel72@gmail.com>
2026-08-13 15:07:04 -0300

    Adiciona configuração Gradle

---
+++ settings.gradle.kts
@@ -0,0 +1,16 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+rootProject.name = "OrganizadorEstoqueV4"
+include(":app")
