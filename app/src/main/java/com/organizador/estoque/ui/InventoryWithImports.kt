7c8d8916aa120f8f1312792f13e04d9eed1e44ad
williamadriel72-code <williamadriel72@gmail.com>
2026-08-13 17:19:56 -0300

    Fix Compose weight import

--- app/src/main/java/com/organizador/estoque/ui/InventoryWithImports.kt
+++ app/src/main/java/com/organizador/estoque/ui/InventoryWithImports.kt
@@ -3,7 +3,6 @@ package com.organizador.estoque.ui
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.weight
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.organizador.estoque.data.InventoryRepository
