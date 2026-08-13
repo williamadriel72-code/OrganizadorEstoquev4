769761898016708cbd7933a9f89448bf346a9c22
williamadriel72-code <williamadriel72@gmail.com>
2026-08-13 19:42:11 -0300

    Remove duplicate PDF bar

--- app/src/main/java/com/organizador/estoque/LauncherActivity.kt
+++ app/src/main/java/com/organizador/estoque/LauncherActivity.kt
@@ -3,23 +3,14 @@ package com.organizador.estoque
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.ui.Modifier
 import com.organizador.estoque.data.InventoryDb
 import com.organizador.estoque.data.InventoryRepository
 import com.organizador.estoque.ui.ModernInventoryApp
-import com.organizador.estoque.ui.PdfImportBar
 
 class LauncherActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val repository = InventoryRepository(InventoryDb(applicationContext))
-        setContent {
-            Column(Modifier.fillMaxSize()) {
-                PdfImportBar(repository)
-                ModernInventoryApp(repository)
-            }
-        }
+        setContent { ModernInventoryApp(repository) }
     }
 }
