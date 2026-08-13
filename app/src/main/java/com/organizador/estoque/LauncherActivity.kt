0b839c1e8c4a5daa2e8ff928e3b4fd6d52bc1df9
williamadriel72-code <williamadriel72@gmail.com>
2026-08-13 19:05:45 -0300

    Launch modern inventory interface

--- app/src/main/java/com/organizador/estoque/LauncherActivity.kt
+++ app/src/main/java/com/organizador/estoque/LauncherActivity.kt
@@ -3,14 +3,23 @@ package com.organizador.estoque
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.ui.Modifier
 import com.organizador.estoque.data.InventoryDb
 import com.organizador.estoque.data.InventoryRepository
-import com.organizador.estoque.ui.InventoryWithImports
+import com.organizador.estoque.ui.ModernInventoryApp
+import com.organizador.estoque.ui.PdfImportBar
 
 class LauncherActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val repository = InventoryRepository(InventoryDb(applicationContext))
-        setContent { InventoryWithImports(repository) }
+        setContent {
+            Column(Modifier.fillMaxSize()) {
+                PdfImportBar(repository)
+                ModernInventoryApp(repository)
+            }
+        }
     }
 }
