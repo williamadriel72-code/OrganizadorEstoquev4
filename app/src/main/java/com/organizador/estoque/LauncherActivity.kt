d7fdb9835685e0502d8972a419a0c201b13303cc
williamadriel72-code <williamadriel72@gmail.com>
2026-08-13 17:13:43 -0300

    Add launcher activity

---
+++ app/src/main/java/com/organizador/estoque/LauncherActivity.kt
@@ -0,0 +1,16 @@
+package com.organizador.estoque
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import com.organizador.estoque.data.InventoryDb
+import com.organizador.estoque.data.InventoryRepository
+import com.organizador.estoque.ui.InventoryWithImports
+
+class LauncherActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val repository = InventoryRepository(InventoryDb(applicationContext))
+        setContent { InventoryWithImports(repository) }
+    }
+}
