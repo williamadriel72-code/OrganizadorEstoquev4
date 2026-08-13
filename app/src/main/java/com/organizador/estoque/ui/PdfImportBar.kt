3c2c63cbc445af9e2558ce8aaf20a42388653101
williamadriel72-code <williamadriel72@gmail.com>
2026-08-13 17:12:11 -0300

    Create PDF import bar

---
+++ app/src/main/java/com/organizador/estoque/ui/PdfImportBar.kt
@@ -0,0 +1,7 @@
+package com.organizador.estoque.ui
+
+import androidx.compose.runtime.Composable
+import com.organizador.estoque.data.InventoryRepository
+
+@Composable
+fun PdfImportBar(repository: InventoryRepository, onImported: () -> Unit = {}) {}
