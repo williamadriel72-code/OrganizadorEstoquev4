04f4144ec5de67b421eabbcfa8a9be691214e03d
williamadriel72-code <williamadriel72@gmail.com>
2026-08-15 18:01:41 -0300

    Bump AWS to 1.0.9 for expiry ANR fix

--- stockmaster/build.gradle.kts
+++ stockmaster/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.aws.gestaoestoque"
         minSdk = 26
         targetSdk = 36
-        versionCode = 10008
-        versionName = "1.0.8"
+        versionCode = 10009
+        versionName = "1.0.9"
     }
 
     buildFeatures {
