8e880bf2cfeee8ac2bfb53085fa30ff2270edaf7
williamadriel72-code <williamadriel72@gmail.com>
2026-08-15 11:46:44 -0300

    Use AWS app router

--- stockmaster/src/main/java/com/stockmaster/clone/MainActivity.kt
+++ stockmaster/src/main/java/com/stockmaster/clone/MainActivity.kt
@@ -3,11 +3,11 @@ package com.stockmaster.clone
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.stockmaster.clone.ui.StockMasterApp
+import com.stockmaster.clone.ui.AwsApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent { StockMasterApp() }
+        setContent { AwsApp() }
     }
 }
