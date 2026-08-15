cc7b805e029ad24b317e01b12fd89e574ed454e5
williamadriel72-code <williamadriel72@gmail.com>
2026-08-15 10:50:21 -0300

    Add StockMaster activity

---
+++ stockmaster/src/main/java/com/stockmaster/clone/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.stockmaster.clone
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import com.stockmaster.clone.ui.StockMasterApp
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent { StockMasterApp() }
+    }
+}
