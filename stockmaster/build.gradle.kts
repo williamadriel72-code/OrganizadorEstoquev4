25271bac870ecf92e7c8d3b21d66b8c14fd0713e
williamadriel72-code <williamadriel72@gmail.com>
2026-08-15 15:58:46 -0300

    Bump premium AWS interface to 1.0.6

--- stockmaster/build.gradle.kts
+++ stockmaster/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.aws.gestaoestoque"
         minSdk = 26
         targetSdk = 36
-        versionCode = 10005
-        versionName = "1.0.5"
+        versionCode = 10006
+        versionName = "1.0.6"
     }
 
     buildFeatures {
