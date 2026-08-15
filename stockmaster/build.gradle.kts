c85b7f317eb5860b7d634b2d8c2a34c8e6357bd5
williamadriel72-code <williamadriel72@gmail.com>
2026-08-15 15:45:17 -0300

    Bump AWS visual redesign to 1.0.5

--- stockmaster/build.gradle.kts
+++ stockmaster/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.aws.gestaoestoque"
         minSdk = 26
         targetSdk = 36
-        versionCode = 10004
-        versionName = "1.0.4"
+        versionCode = 10005
+        versionName = "1.0.5"
     }
 
     buildFeatures {
