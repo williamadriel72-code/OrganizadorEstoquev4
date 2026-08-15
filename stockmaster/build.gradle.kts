251b6e3558fd551d5d84d3b784c1bdf7f9204202
williamadriel72-code <williamadriel72@gmail.com>
2026-08-15 14:44:19 -0300

    Remove PDFBox dependency after removing PDF imports

--- stockmaster/build.gradle.kts
+++ stockmaster/build.gradle.kts
@@ -35,5 +35,4 @@ dependencies {
 
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.10.0")
     implementation("com.google.android.gms:play-services-code-scanner:16.1.0")
-    implementation("com.tom-roush:pdfbox-android:2.0.27.0")
 }
