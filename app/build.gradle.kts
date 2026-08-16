254b571bd12c155b2ee63165d874b3c20fa0a44a
williamadriel72-code <williamadriel72@gmail.com>
2026-08-16 02:09:57 -0300

    Versão 5.0.5 com atualizador interno

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.organizador.estoque"
         minSdk = 26
         targetSdk = 36
-        versionCode = 50005
-        versionName = "5.0.4"
+        versionCode = 50006
+        versionName = "5.0.5"
     }
 
     buildFeatures {
