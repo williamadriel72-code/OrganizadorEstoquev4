787f6ef80467633aa67f7aef633045e9309c2110
williamadriel72-code <williamadriel72@gmail.com>
2026-08-16 00:20:55 -0300

    Atualizar versão para 5.0.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.organizador.estoque"
         minSdk = 26
         targetSdk = 36
-        versionCode = 50003
-        versionName = "5.0.2"
+        versionCode = 50004
+        versionName = "5.0.3"
     }
 
     buildFeatures {
