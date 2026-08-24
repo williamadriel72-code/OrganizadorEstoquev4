5f34b8214d397d297ab9093c1a251bbc553adbf9
williamadriel72-code <williamadriel72@gmail.com>
2026-08-24 11:51:32 -0300

    v1.3: bump versao

--- scanner/build.gradle.kts
+++ scanner/build.gradle.kts
@@ -10,7 +10,7 @@ android {
         applicationId = "com.multifigurinhas.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 3
-        versionName = "1.2.0"
+        versionCode = 4
+        versionName = "1.3.0"
     }
 }
