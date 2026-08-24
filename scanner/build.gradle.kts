05e92b282177553efb3c99f0f9d2ddeed1190eb5
williamadriel72-code <williamadriel72@gmail.com>
2026-08-24 11:25:40 -0300

    Atualiza versão do Multi Figurinhas

--- scanner/build.gradle.kts
+++ scanner/build.gradle.kts
@@ -10,7 +10,7 @@ android {
         applicationId = "com.multifigurinhas.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.1.0"
     }
 }
