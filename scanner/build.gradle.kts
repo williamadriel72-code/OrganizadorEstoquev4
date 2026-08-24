9ba2327795a35e71cba691a2fd4aa582f16e9a8c
williamadriel72-code <williamadriel72@gmail.com>
2026-08-24 12:12:10 -0300

    Bump Multi Figurinhas to 1.4.1

--- scanner/build.gradle.kts
+++ scanner/build.gradle.kts
@@ -10,7 +10,7 @@ android {
         applicationId = "com.multifigurinhas.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 5
-        versionName = "1.4.0"
+        versionCode = 6
+        versionName = "1.4.1"
     }
 }
