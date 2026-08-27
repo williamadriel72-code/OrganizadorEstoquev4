6560be479a2f5d6cc5850c0e13e4d323b3a40a08
williamadriel72-code <williamadriel72@gmail.com>
2026-08-27 09:35:45 -0300

    Bump Android AutoClicker to 1.0.3

--- scanner/build.gradle.kts
+++ scanner/build.gradle.kts
@@ -10,7 +10,7 @@ android {
         applicationId = "com.autoclicker.android"
         minSdk = 26
         targetSdk = 36
-        versionCode = 3
-        versionName = "1.0.2"
+        versionCode = 4
+        versionName = "1.0.3"
     }
 }
