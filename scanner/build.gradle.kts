86b71b65c890b3c9fcb8eb43d3ae53aa891e4a9c
williamadriel72-code <williamadriel72@gmail.com>
2026-08-30 02:14:33 -0300

    Atualiza Bora Michael para v2.1.1

--- scanner/build.gradle.kts
+++ scanner/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.boramichael.hihi"
         minSdk = 26
         targetSdk = 36
-        versionCode = 21
-        versionName = "2.1.0"
+        versionCode = 22
+        versionName = "2.1.1"
     }
 
     buildFeatures {
