678e874bf734b04b46125354cbeb92796e902a95
williamadriel72-code <williamadriel72@gmail.com>
2026-09-14 21:13:48 -0300

    Bump Bora Michael to 2.7.0 for Folgas reminders

--- scanner/build.gradle.kts
+++ scanner/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.boramichael.hihi"
         minSdk = 26
         targetSdk = 36
-        versionCode = 27
-        versionName = "2.6.0"
+        versionCode = 28
+        versionName = "2.7.0"
     }
 
     buildFeatures {
