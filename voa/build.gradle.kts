0adb247a485f8ee6a9640473e36dd5fd3b20a207
williamadriel72-code <williamadriel72@gmail.com>
2026-09-15 20:50:50 -0300

    Cria módulo Android leve do Voa Michael teste AWS

---
+++ voa/build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.organizador.estoque"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "com.organizador.estoque"
+        minSdk = 26
+        targetSdk = 36
+        versionCode = 50017
+        versionName = "5.1.6-voa-test"
+    }
+}
