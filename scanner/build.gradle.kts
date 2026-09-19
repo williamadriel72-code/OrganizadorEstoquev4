134730e934405594a9cc8e86b81bd481a2946182
williamadriel72-code <williamadriel72@gmail.com>
2026-09-19 11:18:31 -0300

    Versao 2.8.3 com interface local robusta

--- scanner/build.gradle.kts
+++ scanner/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.boramichael.hihi"
         minSdk = 26
         targetSdk = 36
-        versionCode = 31
-        versionName = "2.8.2"
+        versionCode = 32
+        versionName = "2.8.3"
     }
 
     buildFeatures {
