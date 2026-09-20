c709163dc40610cac26da343c325fc8048997820
williamadriel72-code <williamadriel72@gmail.com>
2026-09-20 09:27:36 -0300

    Bora Michael 2.9.0: incrementa versão Android

--- scanner/build.gradle.kts
+++ scanner/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.boramichael.hihi"
         minSdk = 26
         targetSdk = 36
-        versionCode = 32
-        versionName = "2.8.3"
+        versionCode = 34
+        versionName = "2.9.0"
     }
 
     buildFeatures {
